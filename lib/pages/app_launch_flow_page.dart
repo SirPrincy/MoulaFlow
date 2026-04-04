30df85d6e53f5fb8a48749c519cdedb66a8b4787
SirPrincy <princy.raherinjatovo@gmail.com>
2026-04-04 16:11:08 +0300

    refactor: remove unused app_access_method import from app_launch_flow_page

--- lib/pages/app_launch_flow_page.dart
+++ lib/pages/app_launch_flow_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:moula_flow/providers.dart';
 
-import '../data/app_access_method.dart';
 import '../home_page.dart';
 import '../security/app_access_gate.dart';
 import 'onboarding_page.dart';
